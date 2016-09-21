47d4867fe450074c71454019030c32aef69128f0
mpahl <mpahl@diw.de>
2016-09-21 14:49:35 +0200

    edit main.py

--- main.py
+++ main.py
@@ -6,6 +6,5 @@ from script.environment import *
 
 convert.main(config, data_csv, data_json)
 d, m = read_tdp("input/test2.csv", "input/test2.json")
-write_stats(d, m, "output/test.json")
-write_stats(d, m, "output/test.yaml", file_type="yaml")
-
+write_stats(d, m, "output/test2.json")
+write_stats(d, m, "output/test2.yaml", file_type="yaml")
