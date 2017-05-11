f6158f5477dfad5c883fabbd5384603fac84a837
mhebing <marcel@ddionrails.org>
2017-05-11 07:19:41 +0200

    Update .gitignore structure and setup.sh

--- setup.sh
+++ setup.sh
@@ -1,27 +1,26 @@
 rm -rf 02-data-processing/input
 mkdir -p 02-data-processing/input
-touch 02-data-processing/input/.gitkeep
 
 rm -rf 02-data-processing/output
 mkdir -p 02-data-processing/output
-touch 02-data-processing/output/.gitkeep
 
 rm -rf 03-data-generation/input
 mkdir -p 03-data-generation/input
-touch 03-data-generation/input/.gitkeep
 
 rm -rf 03-data-generation/output
 mkdir -p 03-data-generation/output
-touch 03-data-generation/output/.gitkeep
 
 rm -rf 04-data-distribution/data
 mkdir -p 04-data-distribution/data
-touch 04-data-distribution/data/.gitkeep
 
 rm -rf 04-data-distribution/generations
 mkdir -p 04-data-distribution/generations
-touch 04-data-distribution/generations/.gitkeep
 
 rm -rf 04-data-distribution/statistics
 mkdir -p 04-data-distribution/statistics
-touch 04-data-distribution/statistics/.gitkeep
+
+rm -rf *temp
+mkdir 01-temp
+mkdir 02-temp
+mkdir 03-temp
+mkdir 04-temp
